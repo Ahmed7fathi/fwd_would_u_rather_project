e690e36fae22489f67d10ac3a0063160aa3c6857
ahmed7fathi <ahmed777fathi@gmail.com>
2021-10-12 20:34:44 +0200

    adding style to NewQuestion component

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -3,8 +3,22 @@ import React, {Component} from 'react'
 class NewQuestion extends Component {
     render() {
         return (
-            <div>
-                <h1>NewQuestion </h1>
+            <div className="new-question-page">
+                <div className="card">
+                    <div className="card-header">
+                        <h2>Create A new Question</h2>
+                    </div>
+                    <div className="card-body">
+                        <p>Complete the Question: </p>
+                        <br/>
+                        <h5>Would you rather ....</h5>
+                        <br/>
+                        <input type="text" placeholder="Enter Option One Text here"/>
+                        <h2 id="or"><span> OR </span></h2>
+                        <input type="text" placeholder="Enter Option One Text here"/>
+                        <button className="new-qs-btn">Submit</button>
+                    </div>
+                </div>
             </div>
         )
     }
