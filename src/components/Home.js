e6fe6194eb1a917e19e336060f47694f4c5da830
ahmed7fathi <ahmed777fathi@gmail.com>
2021-10-13 09:04:28 +0200

    filtering answered & unanswered questions

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,26 +6,59 @@ import QuestionCard from './QuestionCard'
 
 class Home extends Component {
 
+    currentQs = 0;
+
+    changeWindow(n) {
+        this.currentQs = n;
+        this.forceUpdate();
+
+    }
+
     render() {
         const questions = this.props.questions;
 
         return (
-            <div>
-                {
-                    Object.keys(questions).map((question) => {
-                        return <QuestionCard key={questions[question].id} question={questions[question]}/>
-                    })
-                }
+            <div className="home-page">
+                <div className="container">
+                    <button className={`switch-btn ${this.currentQs === 0 ? "active-btn" : ""}`} onClick={() => {
+                        this.changeWindow(0)
+                    }}> Unanswered Questions
+                    </button>
+                    <button className={`switch-btn ${this.currentQs === 1 ? "active-btn" : ""}`} onClick={() => {
+                        this.changeWindow(1)
+                    }}>Answered Questions
+                    </button>
+                    {
+                        this.currentQs === 0 ?
+                            questions.unanswered.map((question) => {
+                                return <QuestionCard key={question.id} question={question}/>
+                            }) :
+                            questions.answered.map((question) => {
+                                return <QuestionCard key={question.id} question={question}/>
+                            })
+                    }
+                </div>
             </div>
         )
     }
 
 }
 
-function mapStateToProps({questions}) {
+function mapStateToProps({authedUser, users, questions}) {
+    const userAnswersIds = Object.keys(users[authedUser].answers);
+
+    const answered = Object.values(questions)
+        .filter((qs) => userAnswersIds.includes(qs.id));
+
+    const unanswered = Object.values(questions)
+        .filter((qs) => !userAnswersIds.includes(qs.id));
+
     return {
-        questions
-    }
+        questions: {
+            answered,
+            unanswered
+        }
+    };
 }
 
 export default connect(mapStateToProps)(Home);
