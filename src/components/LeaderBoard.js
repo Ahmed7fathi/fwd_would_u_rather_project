a81d6dd4931352294950026e91e41371ebb6036c
ahmed7fathi <ahmed777fathi@gmail.com>
2021-10-14 13:31:01 +0200

    adding leaderboard functionality

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,14 +1,70 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+
 
 class LeaderBoard extends Component {
+
     render() {
+        const users = this.props.users_score;
+
         return (
-            <div>
-                <h1>LeaderBoard </h1>
+            <div className="leaderBoard-page">
+                {
+                    users.map(user => (
+
+                        <div className="score-card" key={user.id}>
+
+                            <div className="info">
+                                <div className="img-wrapper">
+                                    <img src={user.avatar} alt={`${user.name} avatar`}/>
+                                </div>
+                                <div className="info-body">
+                                    <h3>
+                                        {user.name}
+                                    </h3>
+                                    <p> Answered questions {user.answers}</p>
+                                    <hr/>
+                                    <p>Created questions {user.questions}</p>
+                                </div>
+                                <div className="total-score">
+                                    <h3 className="header">
+                                        Score
+                                    </h3>
+
+                                    <div className="score-num">
+                                        <p>{user.score}</p>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+
+
+                    ))
+                }
             </div>
         )
     }
 
 }
 
-export default LeaderBoard;
+function mapStateToProps({users}) {
+    const users_score = Object.values(users).map((u) => {
+        return {
+            id: u.id,
+            name: u.name,
+            avatar: u.avatarURL,
+            answers: Object.keys(u.answers).length,
+            questions: u.questions.length,
+            score: Object.keys(u.answers).length + u.questions.length
+        }
+    });
+    users_score.sort((a, b) => {
+        return b.score - a.score
+    });
+    return {
+        users_score
+    }
+}
+
+export default connect(mapStateToProps)(LeaderBoard);
