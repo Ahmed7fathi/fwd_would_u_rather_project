fe6831f8c22a57473e5a177dc23c42eb35b9e570
ahmed7fathi <ahmed777fathi@gmail.com>
2021-10-15 02:27:19 +0200

    adding timestamp to question card

--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -1,19 +1,23 @@
 import React from 'react';
 
+function convertTimestamp(timestamp){
+    return  Date(timestamp)
+}
+
 function questionCard({question, users}) {
     const user_img =  users[question.author].avatarURL;
     return (
         <div className="question-card">
             <div className="qs-header">
-                <h3>{question.author} asks: </h3>
+                <p><span className="username">{question.author}</span> asks : <span className="date">{convertTimestamp(question.timestamp)}</span></p>
             </div>
             <div className="info">
                 <div className="qs-img-wrapper">
                     <img src={user_img} alt={`${question.author} avatar`}/>
                 </div>
                 <div className="info-body">
-                    <h4>Would you rather </h4>
-                    <p>{question.optionOne.text}</p>
+                    <h3>Would you rather </h3>
+                    <h4 className="question">{question.optionOne.text}</h4>
                     <button className="poll-btn">View Poll</button>
                 </div>
             </div>
